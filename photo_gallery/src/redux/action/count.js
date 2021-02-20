73df3fff3efe2b06a5a083509622178fb450049b
ParkJunYong <wnsdyd21@gmail.com>
2021-02-20 23:17:02 +0900

    테스트

--- photo_gallery/src/redux/action/count.js
+++ photo_gallery/src/redux/action/count.js
@@ -4,7 +4,7 @@ export const DECREASE_COUNT = 'count/DECREASE_COUNT';
 
 export const SET_COUNT = 'count/SET_COUNT';
 
-// 커밋
+// 커밋1
 // Action creators
 export const increaseCount = () => {
 	return {
